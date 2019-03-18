2af9f6c68529c8195dd1f24b31e365623ec06ada
Andy Gimma <andy@hylo.com>
2019-03-17 21:54:40 -0500

    Node sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 var path = require('path');
 
 module.exports = {
-  entry: './assets/js/index.js',
+  entry: ['./assets/js/index.js', './assets/stylesheets/index.scss'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+    }]
   }
 };
